fb8877e24c8e5ceea37b65c43b5cc001b3d06556
shimamorieiki <aifore.2270@gmail.com>
2024-08-21 12:54:34 +0900

    fix: pyproject.tomlを捨てる

--- tests/unit/app/function/module_a/test_main_a.py
+++ tests/unit/app/function/module_a/test_main_a.py
@@ -1,10 +1,13 @@
 import sys
+import os
 
 print("test_main_aのsys.path")
+models_path = os.path.abspath(f"{os.getcwd()}/app/function/module_a")
+sys.path.insert(0, models_path)
 for r in sys.path:
     print(r)
 from app.function.module_a.main_a import main
-
+sys.path.remove(models_path)
 
 class TestMainA:
     def test_main(self):
--- tests/unit/app/function/module_b/test_main_b.py
+++ tests/unit/app/function/module_b/test_main_b.py
@@ -2,13 +2,12 @@ import sys
 import os
 
 print("test_main_bのsys.path")
-sys.path.insert(1, os.path.abspath(f"{os.getcwd()}/app/function/module_b"))
-sys.path.remove(
-    r"C:\Users\aifor\prog\practice_pytest\practice_pytest\app\function\module_a"
-)
+models_path = os.path.abspath(f"{os.getcwd()}/app/function/module_b")
+sys.path.insert(0, models_path)
 for r in sys.path:
     print(r)
 from app.function.module_b.main_b import main
+sys.path.remove(models_path)
 
 
 class TestMainB:
