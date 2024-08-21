781a3be5d4fadf209ff87b6fa3b46bb41bad7977
shimamorieiki <aifore.2270@gmail.com>
2024-08-21 16:59:04 +0900

    fix: キャッシュを削除して再importする

--- tests/unit/app/function/supermodule_y/module_a/test_y_main_a.py
+++ tests/unit/app/function/supermodule_y/module_a/test_y_main_a.py
@@ -1,11 +1,12 @@
 import sys
 import os
 
-print("test_y_main_aのsys.path")
+# キャッシュを削除して再importする
+sys.modules.pop("models.model_a")
+
+# 絶対パスを指定する
 models_path = os.path.abspath(f"{os.getcwd()}/app/function/supermodule_y/module_a")
 sys.path.insert(0, models_path)
-for r in sys.path:
-    print(r)
 from app.function.supermodule_y.module_a.main_a import main
 
 sys.path.remove(models_path)
--- tests/unit/app/function/supermodule_y/module_b/test_y_main_b.py
+++ tests/unit/app/function/supermodule_y/module_b/test_y_main_b.py
@@ -1,11 +1,12 @@
 import sys
 import os
 
-print("test_y_main_bのsys.path")
+# キャッシュを削除して再importする
+sys.modules.pop("models.model_b")
+
+# 絶対パスを指定する
 models_path = os.path.abspath(f"{os.getcwd()}/app/function/supermodule_y/module_b")
 sys.path.insert(0, models_path)
-for r in sys.path:
-    print(r)
 from app.function.supermodule_y.module_b.main_b import main
 
 sys.path.remove(models_path)
